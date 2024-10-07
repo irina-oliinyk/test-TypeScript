5e92dce7a6b9ee76bc329920ca1f5b4612c15d10
Iryna Oliinyk <arairishka2@gmail.com>
2024-10-07 21:24:57 +0200

    task 1-5

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,11 @@ enum Role {
   USER,
 }
 
-const person = {
+const person1 = {
   role: Role.ADMIN,
 };
 
-if (person.role === Role.ADMIN) {
+if (person1.role === Role.ADMIN) {
   console.log("Role: ", Role.ADMIN);
 }
 
@@ -28,18 +28,47 @@ for (let item in Test) {
   console.log(item);
 }
 
+// const age = 50;
+// const username = 'Max';
+// const toggle = true;
+// const empty = null;
+// const callback = (a) => { return 100 + a };
+
+// Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
+
+// const age: number = 50;
+// const username: string = "Max";
+// const toggle: boolean = true;
+// const empty: null = null;
+// const callback: (a: number) => number = (a: number) => {
+//   return 100 + a;
+// };
+
+// const callback: (a: number) => number = (a: number) => {
+//   return 100 + a;
+// };
+
+// type Person = {
+//   age: number;
+//   username: string;
+//   toggle: boolean;
+//   empty: null;
+// };
+
 // пример для 1 задания
-type Person = {
-  name: string;
-  age?: number; // age є опціональною властивістю
-};
+// type Person = {
+//   name: string;
+//   age?: number; // age є опціональною властивістю
+// };
 
 //задание 2
+// let person: [string, number] = ["Max", 21];
 
 // Виконуйте це завдання у файлі src/basic/2.ts.
 // У вас є наступний JavaScript масив:
 // let person = ['Max', 21];
 // Як переписати його в TypeScript, використовуючи концепцію кортежів, щоб гарантувати, що перший елемент завжди буде рядком, а другий числом?
+
 // Кортежі зручні, коли нам потрібно зберегти в масив фіксовані значення, наприклад, день, місяць та рік.
 // let date: [number, number, number];
 // date = [7, 11, 2023]; // OK
@@ -50,3 +79,149 @@ type Person = {
 
 // Створіть змінну, яка може містити або рядок, або число (union type)? Також, оголосіть змінну,
 // яка може містити лише одне з двох можливих рядкових значень: 'enable' або 'disable'(literal type).
+
+// let mixedType: string | number;
+
+// let EnableOrDisable: "enable" | "disable";
+// type EnableOrDisable = "enable" | "disable";
+// let value: EnableOrDisable;
+
+// Завдання 4
+
+// Виконуйте це завдання у файлі src/basic/4.ts.
+
+// У вас є такі функції JavaScript:
+
+// function showMessage(message) {
+//   console.log(message);
+// }
+
+// function showMessage(message: string): void {
+//   console.log(message);
+// }
+
+// function calc(num1, num2) {
+//   return num1 + num2;
+// }
+
+// function calc(num1:number , num2:number):number {
+//   return num1 + num2;
+// }
+
+// function customError() {
+//   throw new Error('Error');
+// }
+
+// function customError(): never {
+//   throw new Error("Error");
+// }
+
+// Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
+
+// Завдання 5
+
+// Виконуйте це завдання у файлі src/basic/5.ts.
+// Типізуйте функцію isWeekend яка приймає день тижня з enumDayOfWeek і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
+
+// enum DayOfWeek {
+//   Monday,
+//   Tuesday,
+//   Wednesday,
+//   Thursday,
+//   Friday,
+//   Saturday,
+//   Sunday,
+// }
+
+// const isWeekend = (day: DayOfWeek): boolean => {
+//   return day === DayOfWeek.Saturday || day === DayOfWeek.Sunday;
+// };
+
+// function isWeekend(day: DayOfWeek): boolean {
+//   return day === DayOfWeek.Saturday || day === DayOfWeek.Sunday;
+// }
+
+// console.log(isWeekend(DayOfWeek.Monday));
+// console.log(isWeekend(DayOfWeek.Tuesday));
+// console.log(isWeekend(DayOfWeek.Wednesday));
+// console.log(isWeekend(DayOfWeek.Thursday));
+// console.log(isWeekend(DayOfWeek.Friday));
+// console.log(isWeekend(DayOfWeek.Saturday));
+// console.log(isWeekend(DayOfWeek.Sunday));
+
+// const isWeekend = (day) => {
+// }
+
+// Завдання 6
+
+// Виконуйте це завдання у файлі src/basic/6.ts.
+
+// Створіть інтерфейс User для типізації об'єктів, які містять такі властивості. Зверніть увагу, що адреса є необов'язковою властивістю.
+
+// const mango = {
+//     name: 'Mango',
+//     age: 30,
+//     email: 'john@example.com',
+//     address: {
+//         city: 'New York',
+//         country: 'USA'
+//     }
+// };
+
+// const poly = {
+//     name: 'Mango',
+//     age: 30,
+//     email: 'john@example.com'
+// };
+
+// /////////////////////////////////////////////////////////////////////// //////////////////////////////////
+
+// Завдання 1
+
+const age: number = 50;
+const username: string = "Max";
+const toggle: boolean = true;
+const empty: null = null;
+const callback: (a: number) => number = (a: number) => {
+  return 100 + a;
+};
+
+// Завдання 2
+
+let person: [string, number] = ["Max", 21];
+
+// Завдання 3
+
+let mixedType: string | number;
+
+let EnableOrDisable: "enable" | "disable";
+
+// Завдання 4
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error("Error");
+}
+
+// Завдання 5
+
+enum DayOfWeek {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+const isWeekend = (day: DayOfWeek): boolean => {
+  return day === DayOfWeek.Saturday || day === DayOfWeek.Sunday;
+};
